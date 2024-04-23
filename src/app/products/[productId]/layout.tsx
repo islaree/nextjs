8c4a35acb159226bc9a12a13c688182b323cb877
daxchx <daxchx.v1@gmail.com>
2024-04-23 23:34:37 +0900

    add: nested layouts

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProductsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      <h2>Features products</h2>
+    </>
+  );
+}
