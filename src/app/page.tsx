2c9d913052e07911a100d60732c721da46f8048a
daxchx <daxchx.v1@gmail.com>
2024-04-24 22:43:17 +0900

    link component navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Welcom home!</h1>;
+  return (
+    <>
+      <h1>Welcom home!</h1>
+      <Link href="/products">products</Link>
+    </>
+  );
 }
